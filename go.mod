e51e6900f2787521d419652e1df37e194d0a26fd
Kemal Akkoyun <kakkoyun@gmail.com>
2020-07-18 22:16:26 +0200

    Init applications with metrics
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kakkoyun/observable-remote-write
+
+go 1.14
+
+require (
+	github.com/oklog/run v1.1.0
+	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/common v0.10.0
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ pkg/exthttp/instrumentation.go
@@ -0,0 +1,99 @@
+package exthttp
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// MetricsMiddleware holds necessary metrics to instrument an http.Server
+// and provides necessary behaviors.
+type MetricsMiddleware interface {
+	// NewHandler wraps the given HTTP handler for instrumentation.
+	NewHandler(handlerName string, handler http.Handler) http.HandlerFunc
+}
+
+type nopMetricsMiddleware struct{}
+
+func (ins nopMetricsMiddleware) NewHandler(handlerName string, handler http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// NewNopMetricsMiddleware provides a MetricsMiddleware which does nothing.
+func NewNopMetricsMiddleware() MetricsMiddleware {
+	return nopMetricsMiddleware{}
+}
+
+type defaultMetricsMiddleware struct {
+	requestDuration *prometheus.HistogramVec
+	requestSize     *prometheus.SummaryVec
+	requestsTotal   *prometheus.CounterVec
+	responseSize    *prometheus.SummaryVec
+}
+
+// NewMetricsMiddleware provides default MetricsMiddleware.
+func NewMetricsMiddleware(reg prometheus.Registerer) MetricsMiddleware {
+	ins := defaultMetricsMiddleware{
+		requestDuration: promauto.With(reg).NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name:    "http_request_duration_seconds",
+				Help:    "Tracks the latencies for HTTP requests.",
+				Buckets: []float64{0.001, 0.01, 0.1, 0.3, 0.6, 1, 3, 6, 9, 20, 30, 60, 90, 120},
+			},
+			[]string{"code", "handler", "method"},
+		),
+
+		requestSize: promauto.With(reg).NewSummaryVec(
+			prometheus.SummaryOpts{
+				Name: "http_request_size_bytes",
+				Help: "Tracks the size of HTTP requests.",
+			},
+			[]string{"code", "handler", "method"},
+		),
+
+		requestsTotal: promauto.With(reg).NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "http_requests_total",
+				Help: "Tracks the number of HTTP requests.",
+			}, []string{"code", "handler", "method"},
+		),
+
+		responseSize: promauto.With(reg).NewSummaryVec(
+			prometheus.SummaryOpts{
+				Name: "http_response_size_bytes",
+				Help: "Tracks the size of HTTP responses.",
+			},
+			[]string{"code", "handler", "method"},
+		),
+	}
+
+	return &ins
+}
+
+// NewHandler wraps the given HTTP handler for instrumentation. It
+// registers four metric collectors (if not already done) and reports HTTP
+// metrics to the (newly or already) registered collectors: http_requests_total
+// (CounterVec), http_request_duration_seconds (Histogram),
+// http_request_size_bytes (Summary), http_response_size_bytes (Summary). Each
+// has a constant label named "handler" with the provided handlerName as
+// value. http_requests_total is a metric vector partitioned by HTTP method
+// (label name "method") and HTTP status code (label name "code").
+func (ins *defaultMetricsMiddleware) NewHandler(handlerName string, handler http.Handler) http.HandlerFunc {
+	return promhttp.InstrumentHandlerDuration(
+		ins.requestDuration.MustCurryWith(prometheus.Labels{"handler": handlerName}),
+		promhttp.InstrumentHandlerRequestSize(
+			ins.requestSize.MustCurryWith(prometheus.Labels{"handler": handlerName}),
+			promhttp.InstrumentHandlerCounter(
+				ins.requestsTotal.MustCurryWith(prometheus.Labels{"handler": handlerName}),
+				promhttp.InstrumentHandlerResponseSize(
+					ins.responseSize.MustCurryWith(prometheus.Labels{"handler": handlerName}),
+					handler,
+				),
+			),
+		),
+	)
+}
